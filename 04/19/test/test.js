d9bbf61841a383afa898ff6c82e03dd957b4c5bd
Kim Jueun <jrangkim11@gmail.com>
2022-04-19 17:38:11 +0900

    scss, javascript연습

---
+++ 04/19/test/test.js
@@ -0,0 +1,14 @@
+
+function sum(a, b) {
+  // 매개변수 a와 b는 값이 들어갈 공간이다
+  // 함수는 상자라서 열방법이 없다
+  return a + b;
+  // 반환 = 뱉어낸다
+}
+
+console.log(sum(9,1));
+console.log(sum(5,12));
+console.log(sum(9,5950));
+console.log(sum(84,5));
+console.log(sum(942,95));
+console.log(sum(4,6));
