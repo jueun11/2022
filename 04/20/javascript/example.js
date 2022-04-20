eaae32c919334f43295751c1fa91d92c16e30249
Kim Jueun <jrangkim11@gmail.com>
2022-04-20 16:34:55 +0900

    if 연습

---
+++ 04/20/javascript/example.js
@@ -0,0 +1,50 @@
+// let one = "1";
+//변수선언. 이름은 one , 값은 숫자 타입의 1
+
+// console.log(one);
+// 자바스크립트야 변수 one이 뭔지 모르겠는데, 니 실행창에 기록좀 남겨줘
+// console.log(typeof one);
+// 자스야 변수 one의 데이터탑이 뭔지 모르겠어 이거 뭐야
+
+// let one = "1";
+// let two = 2;
+// console.log(typeof(one + two));
+
+// let father = "아버지가";
+// let room = "방에들어가신다";
+
+// console.log(typeof(father+room);
+//자스야 father라는 변수와 room 이라는 변수를 더한다음에 데이터 타입좀 체크해줘
+
+
+// let sohi = {
+//   name:"sohi",
+//   gender:"female"
+// }
+// console.log(typeof sohi);
+// if(sohi.name === "ukjae") {
+//   console.log("미남이야");
+
+// }
+//   else {
+//     console.log("틀렸어 임마");
+//   }
+
+// let a = 1;
+// let b = 2;
+// let result = "값이 다릅니다"
+// if( a === b ) {
+//   console.log('값이 같습니다');
+// } else {
+//   console.log(result);
+//   //거짓인 경우 변수인 result를 기록하는 행동만 '할 뿐이다' 변수의 값이 무엇인지 관심이 없다
+// }
+
+let a = 1;
+let b = 2;
+let result = "값이 다릅니다"
+if( a !== b ) {
+  console.log('값이 같습니다');
+} else {
+  console.log(result);
+}
