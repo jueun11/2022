22e07710c7debe1ad49f26177cb3a34bb0c54d5e
Kim Jueun <jrangkim11@gmail.com>
2022-04-28 16:56:27 +0900

    자바스크립트 변수 연습

---
+++ 04/28/exam.js
@@ -0,0 +1,13 @@
+function sum (front, back){
+
+  if(typeof front === true){
+
+  }else{
+    console.log("당신이 작성한 front 매개변수는 숫자가 아닙니다");
+  }
+
+  let result = front + back;
+}
+
+console.log(sum("가나",3));
+
