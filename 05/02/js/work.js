17e1f9a033f12ca9db6e4889417e60b47ccd5e7b
Kim Jueun <jrangkim11@gmail.com>
2022-05-02 16:49:43 +0900

    자바스크립트 연습

---
+++ 05/02/js/work.js
@@ -0,0 +1,41 @@
+// // console.log('test');
+
+// let one; // todo : 변수 선언은 했고, 값은 무엇을 '가리키지는' 않는 상태
+
+// //todo: 식별을 위해서 값과 변수를 '대입'하는 과정을 통한다 =(이퀄표시)
+// let test = 1; // ? 변수 선언과 동시에 값을 가리켰다. 초기화되었다.
+// //?  RAM이라는 메모리에 일단 담긴다 
+// // 값은 존재하는 상태
+// // 활용이 아직 안된 상태
+
+// //변수를 왜 쓰는가?
+// //데이터 숫자타입 1이라는 저 겂을 쓸 방법이 없음 -> 변수를 사용하지 않으면
+// console.log(one);
+
+// const two;
+// console.log(two);
+// ? 변수 선언과 동시에 값을 가르켜야한다 const 규칙
+
+// let a = 1;
+// let b = "2";
+
+// let change = Number(b);
+// console.log( a === b);
+// console.log( typeof a ===  typeof b);
+
+// console.log(typeof change);
+
+// let chea = null;
+// // todo : null 존재는 하지만 비어있는 값, 프로그래밍에만 있는 개념 "빈공간"
+// // todo : null 이라는 값이 존재하면 아직 값이 정해지지않았다는 뜻
+// console.log(typeof chea);
+
+// let arr = ['피카츄', '라이츄', '파이리', '꼬부기'];
+
+// console.log(typeof arr);
+// console.log(Array.isArray(arr));
+
+let text = "피곤타";
+
+console.log(text[1],text[2]);
+console.log(text.length);
