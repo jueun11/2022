05139270ff758989dcb646fe483da174451f5187
Kim Jueun <jrangkim11@gmail.com>
2022-05-04 17:44:18 +0900

    자바스크립트 연습

---
+++ 05/04/exam-2.js
@@ -0,0 +1,18 @@
+
+
+function textMachine(startValue, endValue,textValue){
+  for(let i=startValue; i<=endValue; i++){
+    console.log(textValue);
+  }
+}
+
+textMachine(1, 20, "hello");
+textMachine(3, 10, "bye");
+
+
+function b (first, second){
+  console.log(first + second);
+}
+
+b(1,2);
+console.log(b());
---
+++ 05/04/exam-3.js
@@ -0,0 +1,14 @@
+// let a = 1;
+// //글로벌하다
+
+
+// function b () {
+//   // 함수 b 는 목적이 콘솔 찍고 종료하는 기계
+//   // return 값은 없다 -> undefind 값이 리턴된다
+//   // console.log(a);
+//   var c = "이지은";
+//   return c;
+// }
+// //로컬 (for문속 i같은거 리턴업으면 안나오는애들)
+
+// console.log(b());
