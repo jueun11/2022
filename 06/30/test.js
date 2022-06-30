c88cb90c46f7385b839475ddc14ce2e2c1265949
Kim Jueun <jrangkim11@gmail.com>
2022-06-30 14:58:27 +0900

    함수()입력에 따라 달라지는 값

---
+++ 06/30/test.js
@@ -0,0 +1,13 @@
+function one(ipgoo) {
+  if(ipgoo === "바보"){
+    return "나 바보 아닌데?"
+  } else{
+    return "천재";
+  }
+}
+
+
+// console.log(one("바보"));
+//리턴에 적힌거랑 같은 형식만 작동... 스트링은 스트링 넘버는 넘버
+
+console.log(one("바보"));
